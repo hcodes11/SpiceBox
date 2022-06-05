beffedc9e379b3effa61fe5aa904b2415833b327
harika <hcodes@gmail.com>
2022-06-05 14:46:24 -0600

    Authentication

--- spiceboxclient/src/Switch.js
+++ spiceboxclient/src/Switch.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './views/Home';
 import NewRecipe from './views/NewRecipe';
+import Details from './views/Details';
 
 export default function Switch() {
     return (
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/recipe' element={<NewRecipe />} />
+        <Route path='/details' element={<Details />} />
       </Routes>
   );
 }
---
+++ spiceboxclient/src/views/SignIn.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { signInUser } from '../api/data/auth/firebaseLogin';
+
+export default function SignIn() {
+
+  return (
+    <>
+      <div className="text-center mt-5">
+        <h1>Welcome to Spice Box!</h1>
+        <h2>Developed by Harika</h2>
+        <button type="button" className="btn btn-success" onClick={signInUser}>
+          Login to add your Recipes!
+        </button>
+      </div>
+    </>
+  );
+}
